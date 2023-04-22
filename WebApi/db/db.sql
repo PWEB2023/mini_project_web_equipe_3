e496a9d00d5ea7c075a1851fae362e5896055126
PWEB2023 <124344342+PWEB2023@users.noreply.github.com>
2023-04-22 15:52:28 +0100

    Delete WebApi directory

--- WebApi/db/db.sql
+++
@@ -1,8 +0,0 @@
---
--- Table structure for table `products`
---
-
-
-INSERT INTO `application` (`app_id`, `app_name`, `app_desc`, `app_image`,`app_download`) VALUES
-  (1, 'Visual Studio Code', 'Visual Studio Code is a code editor redefined and optimized','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfx4gjsCOY7sV99VWjTqhASOuOgDJmMxXb1edrm46fNH78gq7mm3gxeN3Q3eK7Ifh9Ng&usqp=CAU','https://az764295.vo.msecnd.net/stable/92da9481c0904c6adfe372c12da3b7748d74bdcb/VSCodeUserSetup-x64-1.76.0.exe'),
-  (2, 'Code bloks', 'Code::Blocks is a free C/C++ and Fortran IDE built to meet the most demanding needs of its users. It is designed to be very extensible and fully configurable','https://pic.clubic.com/v1/images/1506483/raw','https://netix.dl.sourceforge.net/project/codeblocks/Binaries/20.03/Windows/codeblocks-20.03mingw-setup.exe');
